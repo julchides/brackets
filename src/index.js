a244777aaffaad1a073cf7012c96d035fa76cb83
julchides <jul.chi.des@gmail.com>
2023-01-20 20:01:55 +0200

    feat: brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = bracketsConfig.map((x) => x[0]);
+  const pairBrackets = Object.fromEntries(bracketsConfig);
+
+  let stackBrackets = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+    let topBracket = stackBrackets[stackBrackets.length - 1];
+
+    if (openBrackets.includes(currentBracket)) {
+      if (pairBrackets[currentBracket] === currentBracket) {
+        if (currentBracket === topBracket) {
+          stackBrackets.pop();
+        }
+      }
+      stackBrackets.push(currentBracket);
+    } else if (stackBrackets.length === 0) {
+      return false;
+    }
+
+    if (pairBrackets[topBracket] === currentBracket) {
+      stackBrackets.pop();
+    } else if (stackBrackets.length === 0) {
+      return false;
+    }
+  }
+
+  return stackBrackets.length === 0;
+};
